4ba4d9d734fcf7c3ad8d772392fabea7cac6894a
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-17 20:44:00 +0100

    Updated 0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 /* more headers goes there */
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 /* betty style doc for function main goes there */
 int main(void)
 {
