b076f0b9b99da769e4d786ccd6cca66556fbd91f
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-17 22:22:41 +0100

    Added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
