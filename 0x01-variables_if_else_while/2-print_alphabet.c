3193b5ecb98b10d1224b51a936119d4c75a16c73
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-17 21:16:53 +0100

    Added 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        /* code */
+        putchar(c);
+    }
+    putchar('\n');
+
+    return (0);
+}
