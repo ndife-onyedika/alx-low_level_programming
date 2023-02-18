a645ad18151c409a7caf1fd5aceb1262aa38767c
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-18 02:39:50 +0100

    Updated 100-print_comb3.c,  101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,9 +12,12 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	for (int c = 0; c < 9; c++)
+	int c;
+	int d;
+
+	for (c = 0; c < 9; c++)
 	{
-		for (int d = c + 1; d < 10; d++)
+		for (d = c + 1; d < 10; d++)
 		{
 			putchar(c + '0');
 			putchar(d + '0');
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,11 +12,14 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	for (int c = 0; c < 8; c++)
+	int c;
+	int d;
+	int e;
+	for (c = 0; c < 8; c++)
 	{
-		for (int d = c + 1; d < 9; d++)
+		for (d = c + 1; d < 9; d++)
 		{
-			for (int e = d + 1; e < 10; e++)
+			for (e = d + 1; e < 10; e++)
 			{
 				/* code */
 				putchar(c + '0');
