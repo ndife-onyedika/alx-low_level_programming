ecd8f187e3deb02fbf2f886ac22825d64cbafc4a
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-22 05:22:34 +0100

    Updated 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,7 +13,9 @@ int print_last_digit(int n)
 {
 	int last_num;
 
-	last_num = abs(n) % 10;
+	last_num = n % 10;
+	if (last_num < 0)
+		last_num *= -1;
 	_putchar(last_num + '0');
 
 	return (last_num);
