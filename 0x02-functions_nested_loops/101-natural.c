811cb3b92f44791fadf2755a3417021f65324f0b
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-22 07:48:19 +0100

    Updated 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,19 +6,19 @@
  */
 int main(void)
 {
-  int i;
-  int sum = 0;
+	int i;
+	int sum = 0;
 
-  for (i = 0; i < 1024; i++)
-  {
-    /* code */
-    if (i % 3 == 0 || i % 5 == 0)
-    {
-      /* code */
-      sum += i;
-    }
-  }
-  printf("%d\n", sum);
+	for (i = 0; i < 1024; i++)
+	{
+		/* code */
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			/* code */
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
 
-  return (0);
+	return (0);
 }
