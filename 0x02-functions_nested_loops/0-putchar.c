c2c121df4f6422515737ad7c87fe096f720f462e
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-22 03:27:06 +0100

    Updated 0-putchar.c, Added main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
-
+#include <unistd.h>
+/**
+ * main - Entry point
+ * print "_putchar" with write
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	/* code */
-	printf("_putchar\n");
+	write(1, "_putchar\n", 10);
 	return (0);
 }
