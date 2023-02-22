95e36ce00de923f6bc0abc299c65c6143b359981
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-22 05:35:54 +0100

    Added 10-add.c, Updated main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Add two integer
+ * @x: integer 1
+ * @y: integer 2
+ * Return: returns nothing
+ */
+int add(int x, int y)
+{
+	int _add = x + y;
+
+	return (_add + '0');
+}
