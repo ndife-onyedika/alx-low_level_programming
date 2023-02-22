5eaeae1de16e81e0ee1cb3557fca04edc4cd335b
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-22 05:59:00 +0100

    Updated 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,37 +7,6 @@
  */
 void print_to_98(int n)
 {
-	// int i;
-
-	// if (n < 98)
-	// {
-	// 	for (i = n; i <= 98; i++)
-	// 	{
-	// 		/* code */
-	// 		if (i != 98)
-	// 			printf("%d, ", i);
-	// 		else
-	// 			printf("%d ", i);
-	// 	}
-	// 	_putchar('\n');
-	// }
-	// else if (n > 98)
-	// {
-	// 	/* code */
-	// 	for (i = n; i >= 98; i--)
-	// 	{
-	// 		/* code */
-	// 		if (i != 98)
-	// 			printf("%d, ", i);
-	// 		else
-	// 			printf("%d ", i);
-	// 	}
-	// 	_putchar('\n');
-	// }
-	// else if (n == 98)
-	// {
-	// 	printf("%d\n", n);
-	// }
 	if (n >= 98)
 	{
 		while (n > 98)
