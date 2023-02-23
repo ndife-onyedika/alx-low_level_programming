a9bd0148f40064b977220176719440135031bb2d
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-23 23:19:16 +0100

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - check if character is upper case
+ *
+ * @a: integer argument
+ * @b: integer argument
+ *
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+	int _mul;
+
+	_mul = a * b;
+
+	return (_mul);
+}
