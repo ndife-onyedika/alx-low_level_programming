3c84dde8e3a273a613be648ff10ff43e4e595958
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-24 18:25:01 +0100

    Added 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_line - draws straight line in the terminal
+ *
+ * Return: 0 or 1
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		/* code */
+		for (i = 0; i <= n; i++)
+		{
+			/* code */
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
