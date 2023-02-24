21d19002792b30f8a72eb7245ef82c6f14bb6872
Onyedika Ndife <onyedikandife@gmail.com>
2023-02-24 20:01:38 +0100

    Updated 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,25 +8,25 @@
  */
 void print_diagonal(int n)
 {
-  int i, j;
+	int i, j;
 
-  if (n > 0)
-  {
-    /* code */
-    for (i = 0; i < n; i++)
-    {
-      /* code */
-      for (j = 0; j <= i; j++)
-      {
-        /* code */
-        _putchar(' ');
-      }
-      _putchar('\\');
-      _putchar('\n');
-    }
-  }
-  else
-  {
-    _putchar('\n');
-  }
+	if (n > 0)
+	{
+		/* code */
+		for (i = 0; i < n; i++)
+		{
+			/* code */
+			for (j = 0; j <= i; j++)
+			{
+				/* code */
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
