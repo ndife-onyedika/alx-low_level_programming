cb7e576a9af822c40bc91efed1b23eafb01b679a
Onyedika Ndife <onyedikandife@gmail.com>
2023-03-18 00:42:01 +0100

    Updated 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,11 +10,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+	s1 = s1 != NULL ? s1 : "";
+	s2 = s2 != NULL ? s2 : "";
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	while (s1[len1])
 		len1++;
 	while (s2[len2])
