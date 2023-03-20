cfde2d42a5640bc90a06454f9468a05824af0723
Onyedika Ndife <onyedikandife@gmail.com>
2023-03-21 00:34:22 +0100

    Added 1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog -  function
+ * @d: struct
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	/* code */
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
