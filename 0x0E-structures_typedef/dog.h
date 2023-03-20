8451ac04f6eda14c0a1a96313624658cb350fefc
Onyedika Ndife <onyedikandife@gmail.com>
2023-03-21 00:26:15 +0100

    Added 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	/* data */
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
