58a8830b167308cb23dfe2d96ca066f3f858d119
Onyedika Ndife <onyedikandife@gmail.com>
2023-03-21 01:38:02 +0100

    Added 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - a function that prints a struct dog
+ * @d: dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name != NULL ? d->name : "(nil)");
+	if (d->age == 0)
+		printf("(nil)");
+	else
+		printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner != NULL ? d->owner : "(nil)");
+}
